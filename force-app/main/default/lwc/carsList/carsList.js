9ba5d789fb04d2048d48aa1528dda1c4220f4155
Руслан <ruslanhlukhov@gmail.com>
2022-12-24 16:18:59 +0200

    Button for order car by capacity

--- force-app/main/default/lwc/carsList/carsList.js
+++ force-app/main/default/lwc/carsList/carsList.js
@@ -3,6 +3,7 @@ import getCars from '@salesforce/apex/carsListController.getCars';
 import getCarsByModel from '@salesforce/apex/carsListController.getCarsByModel';
 import Search from '@salesforce/label/c.Search';
 import ModelCar from '@salesforce/label/c.Model';
+import SortByCapacity from '@salesforce/apex/carsListController.getCarsByEngineCapacity';
 export default class CarsList extends LightningElement {
 
     label = {
@@ -45,6 +46,33 @@ export default class CarsList extends LightningElement {
             .catch(error => {
                 this.error = error;
             });
+    }
+
+    loadCarByCapacity(){
+        console.log('testCarByCapasity');
+        SortByCapacity()
+        .then(result => {
+            this.showData = false;
+            this.recordsList = [];
+            for (let key in result) {
+                let fotoUrl = 'https://resourceful-bear-swh5o0-dev-ed--c.documentforce.com/servlet/servlet.FileDownload?file=' + result[key].attachmentId;
+                this.recordsList.push({
+                    Name: result[key].product.Name,
+                    Model: result[key].product.Model__c,
+                    Build: result[key].product.Build_Data__c,
+                    EngineCapacity: result[key].product.Engine_capacity__c,
+                    Price: result[key].product.PriceUSD__c,
+                    Vin: result[key].product.Vin_Code__c,
+                    Photo: fotoUrl,
+                    PriceUa: result[key].uaPrice
+                });            
+            }
+            this.showData = true;
+        })
+        .catch(error => {
+            this.error = error;
+        });
+        
     }
 
     handleKeyChange(event) {
